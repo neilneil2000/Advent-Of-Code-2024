72f207e90837bd708f4a16475914027c234b2da0
Neil Jeffery <neil.jeffery@bt.com>
2025-01-14 18:40:54 +0000

    Day 1 Part 2 Complete

--- Day1/day1.py
+++ Day1/day1.py
@@ -3,7 +3,7 @@ from day1_input import DAY_1_INPUT
 
 def process_data():
     list_a, list_b = zip(*map(lambda x: x.split(), DAY_1_INPUT.splitlines()))
-    return map(int, list_a), map(int, list_b)
+    return list(map(int, list_a)), list(map(int, list_b))
 
 
 def main():
@@ -11,6 +11,9 @@ def main():
     list_a, list_b = process_data()
     ans = sum(map(lambda x, y: abs(x - y), sorted(list_a), sorted(list_b)))
     print(f"Day 1 Part 1: {ans}")
+
+    ans = sum(map(lambda x: list_b.count(x) * x, list_a))
+    print(f"Day 1 Part 2: {ans}")
 
 
 if __name__ == "__main__":
