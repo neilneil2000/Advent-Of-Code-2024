fa437a4fd294ca835316da65c2a6b16f2ac0ab35
Neil Jeffery <neil.jeffery@bt.com>
2025-01-17 16:30:20 +0000

    Day 6 OOP

--- Day6/day6.py
+++ Day6/day6.py
@@ -1,27 +1,14 @@
 import operator
 
 from day6_input import main_map
+from guard_map import GuardMap
 
 
 def main():
-    directions = [(0, -1), (1, 0), (0, 1), (-1, 0)]
-    index = 0
     my_map = list(map(list, main_map))
-    for y, row in enumerate(my_map):
-        for x, cell in enumerate(row):
-            if cell == "^":
-                position = (x, y)
-    try:
-        while True:
-            while my_map[position[1]][position[0]] != "#":
-                my_map[position[1]][position[0]] = "X"
-                position = tuple(map(operator.add, position, directions[index]))
-            position = tuple(map(operator.sub, position, directions[index]))
-            index = (index + 1) % 4
-    except IndexError:
-        print("Out of Bounds")
-
-    print(sum(map(lambda x: x.count("X"), my_map)))
+    guard_map = GuardMap(my_map)
+    guard_map.solve()
+    print(guard_map.path_length())
 
 
 if __name__ == "__main__":
--- Day6/guard_map.py
+++ Day6/guard_map.py
@@ -1,8 +1,12 @@
+import operator
+
+
 class GuardMap:
-    def __init__(self, layout: list[str]):
+    def __init__(self, layout: list[list[str]]):
         self.__layout = layout
         self.__position = self.get_start()
         self.__directions = [(0, -1), (1, 0), (0, 1), (-1, 0)]
+        self.__arrows = ["^", ">", "v", "<"]
         self.__index = 0
 
     def get_start(self) -> tuple[int, int]:
@@ -18,9 +22,20 @@ class GuardMap:
     def direction(self) -> tuple[int, int]:
         return self.__directions[self.__index]
 
-    def move(self):
-        while self.__position != "#":
-            x, y = self.__position
-            self.__layout[y][x] = "X"
-    
-    def get_
+    def solve(self):
+        try:
+            while True:
+                while self.__layout[self.__position[1]][self.__position[0]] != "#":
+                    self.__layout[self.__position[1]][self.__position[0]] = "X"
+                    self.__position = tuple(
+                        map(operator.add, self.__position, self.direction)
+                    )
+                self.__position = tuple(
+                    map(operator.sub, self.__position, self.direction)
+                )
+                self.turn()
+        except IndexError:
+            print("Out of Bounds")
+
+    def path_length(self):
+        return sum(map(lambda x: x.count("X"), self.__layout))
