76d04a07eb0b60c4f06a1d4826bfb64a1c06bfb5
Neil Jeffery <neil.jeffery@bt.com>
2025-01-20 17:25:11 +0000

    Day 7 Part 2 Complete

--- Day7/day7.py
+++ Day7/day7.py
@@ -14,10 +14,18 @@ def main():
 
 def is_valid(target, operands):
     if len(operands) > 2:
-        return is_valid(target, [operands[0] + operands[1]] + operands[2:]) or is_valid(
-            target, [operands[0] * operands[1]] + operands[2:]
+        return (
+            is_valid(target, [operands[0] + operands[1]] + operands[2:])
+            or is_valid(target, [operands[0] * operands[1]] + operands[2:])
+            or is_valid(
+                target, [int(str(operands[0]) + str(operands[1]))] + operands[2:]
+            )
         )
-    return target in [operands[0] + operands[1], operands[0] * operands[1]]
+    return target in [
+        operands[0] + operands[1],
+        operands[0] * operands[1],
+        int(str(operands[0]) + str(operands[1])),
+    ]
 
 
 if __name__ == "__main__":
