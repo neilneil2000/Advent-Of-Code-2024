f842083c3fe3f31a4b27307b88bd1fb9ca8ae6a7
Neil Jeffery <neil.jeffery@bt.com>
2025-01-20 16:55:56 +0000

    Day 6 Part 2 Complete

--- Day6/day6.py
+++ Day6/day6.py
@@ -1,5 +1,6 @@
 from day6_input import main_map
 from guard_map import GuardMap
+import copy
 
 
 def main():
@@ -7,6 +8,21 @@ def main():
     guard_map = GuardMap(my_map)
     guard_map.part_1()
     print(f"Day 6 Part 1: {guard_map.path_length()}")
+    potential_obstacles = guard_map.path_values()
+    total = 0
+    for obstacle in potential_obstacles:
+        new_map = list(map(list, main_map))
+        x, y = obstacle
+        new_map[y][x] = "#"
+        validator = GuardMap(new_map)
+        validator.part_1()
+        if validator.in_bounds() and validator.is_loop():
+            total += 1
+    print(total)
+
+    # my_map = list(map(list, main_map))
+    # guard_map = GuardMap(my_map)
+    # print(f"Day 6 Part 2: {guard_map.part_2()}")
 
 
 if __name__ == "__main__":
--- Day6/guard_map.py
+++ Day6/guard_map.py
@@ -2,18 +2,24 @@ import operator
 
 
 class GuardMap:
-    def __init__(self, layout: list[list[str]]):
+    def __init__(self, layout: list[list[str]], obstacle_step=None):
         self.__layout = layout
-        self.__position = self.get_start()
+        self.obstacle_step = obstacle_step
+        self.__start = self.get_start()
+        self.__position = self.__start
         self.__directions = [(0, -1), (1, 0), (0, 1), (-1, 0)]
         self.__arrows = ["^", ">", "v", "<"]
         self.__index = 0
+        self.my_obstacles = set()
+        self.__step_counter = 0
+        self.path = [self.__position]
 
     def get_start(self) -> tuple[int, int]:
         """Locate starting position"""
         for y, row in enumerate(self.__layout):
             for x, cell in enumerate(row):
-                if cell == "^":
+                if cell in ["^", {"^"}]:
+                    self.__layout[y][x] = {"^"}
                     return (x, y)
 
     def turn(self):
@@ -26,16 +32,47 @@ class GuardMap:
         return self.__directions[self.__index]
 
     def part_1(self):
-        """Execute Part 1 Solution"""
+        """
+        Execute Part 1 Solution
+        """
         while True:
-            while not self.is_obstacle():
-                self.lay_trace()
-                self.forward()
-                if not self.in_bounds():
+            self.forward()
+            if not self.in_bounds() or self.is_loop():
+                return
+            if (
+                self.obstacle_step is not None
+                and self.obstacle_step == self.__step_counter
+            ):
+                if self.in_free_space():
+                    self.place_obstacle()
+                else:
+                    self.__position = (-1, -1)
+                    self.obstacle_step = None
                     return
-            self.backward()
-            self.turn()
+            if self.is_obstacle():
+                self.backward()
+                self.turn()
             self.lay_trace()
+
+    def path_values(self):
+        path = set(self.path)
+        path.remove(self.__start)
+        return path
+
+    def in_free_space(self):
+        return self.__layout[self.__position[1]][self.__position[0]] == "."
+
+    def place_obstacle(self):
+        self.__layout[self.__position[1]][self.__position[0]] = "#"
+        self.my_obstacles.add(self.__position)
+        self.obstacle_step = None
+
+    def is_loop(self):
+        """True if loop detected"""
+        return (
+            self.__arrows[self.__index]
+            in self.__layout[self.__position[1]][self.__position[0]]
+        )
 
     def in_bounds(self):
         "True if __position is in bounds"
@@ -52,6 +89,7 @@ class GuardMap:
             self.__layout[self.__position[1]][self.__position[0]] = {
                 self.__arrows[self.__index]
             }
+        self.path.append(self.__position)
 
     def is_obstacle(self):
         """Return True if __position is obstacle"""
@@ -69,7 +107,9 @@ class GuardMap:
     def forward(self):
         """Move Forward one position"""
         self.__position = tuple(map(operator.add, self.__position, self.direction))
+        self.__step_counter += 1
 
     def backward(self):
         "Move backwards one position"
         self.__position = tuple(map(operator.sub, self.__position, self.direction))
+        self.__step_counter -= 1
