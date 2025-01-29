f2c5fed2795f517f5c1181c4e5421ed192cfbd66
Neil Jeffery <nwjeffery@gmail.com>
2025-01-29 21:55:38 +0000

    Day 11 Part 2 Complete

--- Day11/day11.py
+++ Day11/day11.py
@@ -1,11 +1,18 @@
 def main():
     stone_list = "572556 22 0 528 4679021 1 10725 2790".split()
     stone_list = list(map(int, stone_list))
+    stone_list = {stone: 1 for stone in stone_list}
+
     my_stones = Stones(stone_list)
-    print(my_stones.stones)
     for i in range(25):
         my_stones.blink()
-        print(f"{i + 1}:{len(my_stones.stones)}")
+
+    print(f"Day 11 Part 1: {sum(my_stones.stones.values())}")
+
+    my_stones = Stones(stone_list)
+    for i in range(75):
+        my_stones.blink()
+    print(f"Day 11 Part 2: {sum(my_stones.stones.values())}")
 
 
 class Stones:
@@ -21,10 +28,17 @@ class Stones:
         return [stone * 2024]
 
     def blink(self):
-        new_stones = []
-        for stone in self.stones:
-            new_stones.extend(self.process_stone(stone))
-        self.stones = new_stones
+        if isinstance(self.stones, list):
+            new_stones = []
+            for stone in self.stones:
+                new_stones.extend(self.process_stone(stone))
+            self.stones = new_stones
+        elif isinstance(self.stones, dict):
+            new_stones = {}
+            for stone, count in self.stones.items():
+                for stone in self.process_stone(stone):
+                    new_stones[stone] = new_stones.get(stone, 0) + count
+            self.stones = new_stones
 
 
 if __name__ == "__main__":
