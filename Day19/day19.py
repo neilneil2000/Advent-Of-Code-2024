baa77abdace9ec48b02511644c3d52141729b646
Neil Jeffery <neil.jeffery@bt.com>
2025-02-21 13:56:18 +0000

    Day 19 Part 2 Complete

--- Day19/day19.py
+++ Day19/day19.py
@@ -1,3 +1,4 @@
+from functools import lru_cache
 from day19_input import designs, towels
 
 # from day19_input import designs_example as designs
@@ -16,10 +17,27 @@ def chop_towel(towel, max_len):
     return []
 
 
+@lru_cache()
+def all_combos(towel, max_len):
+    """Return all combinations of towel patterns"""
+    total = 0
+    for x in range(min(max_len, len(towel)), 0, -1):
+        if towel[:x] not in designs:
+            continue
+        if not towel[x:]:
+            total += 1
+        if remainder := all_combos(towel[x:], max_len):
+            total += remainder
+    return total
+
+
 def main():
     max_len = max(len(design) for design in designs)
     counter = sum(chop_towel(towel, max_len) != [] for towel in towels)
     print(f"Day 19 Part 1: {counter}")
+
+    total = sum(all_combos(towel, max_len) for towel in towels)
+    print(f"Day 19 Part 2: {total}")
 
 
 if __name__ == "__main__":
